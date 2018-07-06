56e85f165a53e9acabfec398358efdc23a0f017d
albert752 <azemar.albert@gmail.com>
2018-07-07 00:52:38 +0200

    Adding basic power calculations in caracterManagement.py and testing the formulas through octave

--- tools/characterManagement.py
+++ tools/characterManagement.py
@@ -78,6 +78,7 @@ def _addManual(name, force, template):
                 template[key] = name
             elif(type(template[key]) == str):
                 template[key] = input("Enter " +key+ " value: ")
+    template["ZPower"] = calcZPower(template)
     pp(template)
     return {name: template}
 
@@ -87,20 +88,79 @@ def _addAuto():
     pass
 
 
+def calcZPower(character):
+    '''
+    Given a character dict, return  the character z power following the formula
+    (hp/15) + SATK + BATK+ SDEF + BDEF + ((crit+ki+valish)/100)*1000
+
+    args:
+        character: desired character's dict
+    returns:
+        int: ZPower
+    '''
+    baseUsrStats = 0
+    for key in character["UserStats"]:
+        baseUsrStats = baseUsrStats + character["UserStats"][key]
+    baseUsrStats = baseUsrStats - character["UserStats"]["HP"]
+
+    baseSoulStats = 0
+    for key in character["SoulBoost"]:
+        baseSoulStats = baseSoulStats + character["SoulBoost"][key]
+    baseSoulStats = baseSoulStats - character["SoulBoost"]["HP"]
+
+    multi = _starsToPercentage(character["Stars"])
+    statsSum = (baseUsrStats + baseSoulStats)*multi
+    hp = (character["SoulBoost"]["HP"] + character["UserStats"]["HP"])*multi
+
+    otherUserSum = 0
+    for key in character["OtherUserStats"]:
+        otherUserSum = otherUserSum + character["OtherBaseStats"][key]
+
+    otherSoulBoost = 0
+    for key in character["OtherSoulBoost"]:
+        otherSoulBoost = otherSoulBoost + character["OtherUserStats"][key]
+
+    otherSum = (otherUserSum + otherSoulBoost)*multi
+
+    ZPower = (hp/15) + statsSum + ((otherSum+2000)/100)*character["Level"]
+    return ZPower
+
+
+def _starsToPercentage (numStars):
+    if numStars == 0:
+        return 1
+    elif numStars == 1:
+        return 1.05
+    elif numStars == 2 or numStars == 3:
+        return 1.1
+    elif numStars == 4 or numStars == 5:
+        return 1.15
+    elif numStars == 6 or numStars == 7:
+        return 1.2
+    else:
+        return None
+
+
+
 if __name__ == "__main__":
-    print("Welcome to the manual updater")
-    file = open("./databases/baseCharacters.json", "r")
-    characters = json.load(file)
-    pp(characters)
-    file.close()
-    cont = True
+    # print("Welcome to the manual updater")
+    # file = open("./databases/baseCharacters.json", "r")
+    # characters = json.load(file)
+    # pp(characters)
+    # file.close()
+    # cont = True
 
-    while(cont):
-        aux = addCharacter(force = True)
-        characters.update(aux)
-        pp(characters)
-        cont = bool(input("Continue?  "))
+    # while(cont):
+        # aux = addCharacter(force = True)
+        # characters.update(aux)
+        # pp(characters)
+        # cont = bool(input("Continue?  "))
 
-    file = open("./databases/baseCharacters.json", "w")
-    json.dump(characters, file, sort_keys = True, indent = 4, ensure_ascii = False)
+    # file = open("./databases/baseCharacters.json", "w")
+    # json.dump(characters, file, sort_keys = True, indent = 4, ensure_ascii = False)
+    # file.close()
+    file = open("./templates/character.json", "r")
+    character = json.load(file)
+    pp(character)
     file.close()
+    pp(calcZPower(character))
